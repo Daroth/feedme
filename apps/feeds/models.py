2d7ff200e7a6903f82cdb2df3483308c18c67009
MatToufoutu <mattoufootu@gmail.com>
2011-02-03 22:51:48 +0100

    created models

--- apps/feeds/models.py
+++ apps/feeds/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Section(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Feed(models.Model):
+    url = models.URLField(unique=True)
+    favicon = models.URLField(blank=True)
+    last_update = models.DateTimeField(auto_now=True)
+    title = models.CharField(unique=True, max_length=50)
+    description = models.CharField(max_length=150, blank=True, null=True)
+    section = models.ForeignKey(Section)
+
+    def __unicode__(self):
+        return self.url
+
+
+class Post(models.Model):
+    feed = models.ForeignKey(Feed, to_field='title')
+    link = models.URLField(unique=True, verify_exists=False)
+    title = models.CharField(max_length=150)
+    summary = models.TextField(unique=True)
+    published = models.DateTimeField(blank=True)
+    is_read = models.BooleanField()
+
+    def __unicode__(self):
+        return self.title
