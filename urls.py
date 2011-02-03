89d35d804b9ed4a1380a68b0bcd62040bb04996b
MatToufoutu <mattoufootu@gmail.com>
2011-02-03 19:20:02 +0100

    initial commit

---
+++ urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        })
+    )
